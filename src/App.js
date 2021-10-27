eef22dd22a68707c86fb8bbace14c1376e7c6a37
Denis Valdivieso <sshshaka@gmail.com>
2021-10-27 09:39:45 -0400

    Init
    + dependency - uuid
    + structure
    + styles index.css, normalize, skeleton
    + fonts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Administrador de Citas</h1>
+      <div className="container">
+        <div className="one-half column">
+          <h2>Formulario</h2>
+        </div>
+        <div className="one-half column">
+          <h2>Lista</h2>
+        </div>
+      </div>
+    </>
   );
 }
 
